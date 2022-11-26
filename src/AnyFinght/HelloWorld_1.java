34b3651580341449bff023a90f19a492e02bdb88
Danissimmo <danila20032302@yandex.ru>
2022-11-26 19:27:22 +0300

    Добавлено HelloWorld_1 and HelloWorld_2

---
+++ src/AnyFinght/HelloWorld_1.java
@@ -0,0 +1,7 @@
+package AnyFinght;
+
+public class HelloWorld_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/AnyFinght/HelloWorld_2.java
@@ -0,0 +1,7 @@
+package AnyFinght;
+
+public class HelloWorld_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world 2");
+    }
+}
