caaf75455c859d5f7793121f461d85e0d0b888e7
Danissimmo <danila20032302@yandex.ru>
2022-11-26 19:46:42 +0300

    Добавили класс Suum и обновили HelloWorld_1

--- src/AnyFinght/HelloWorld_1.java
+++ src/AnyFinght/HelloWorld_1.java
@@ -2,6 +2,8 @@ package AnyFinght;
 
 public class HelloWorld_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Suum suum = new Suum();
+        System.out.println("Hello world = 55" +suum.SuumReturn());
+
     }
 }
---
+++ src/AnyFinght/Suum.java
@@ -0,0 +1,7 @@
+package AnyFinght;
+
+public class Suum {
+    int SuumReturn(){
+        return 2+2;
+    }
+}
